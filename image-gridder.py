f941d9c25842c94db2761f56ed150e126b8acef1
jappoman <gabriele.tarenzi@gmail.com>
2023-07-22 10:02:41 +0200

    added readme

--- image-gridder.py
+++ image-gridder.py
@@ -2,58 +2,58 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 
 def draw_grid(image_path):
-    # Apri l'immagine originale
+    # Open the original image
     original_image = Image.open(image_path)
 
-    # Ottieni la risoluzione dell'immagine in DPI (punti per pollice)
+    # Get the image resolution in DPI (dots per inch)
     dpi = original_image.info.get('dpi')
 
-    # Calcola il numero di pixel per centimetro in base ai DPI
+    # Calculate the number of pixels per centimeter based on the DPI
     pixels_per_cm = dpi[0] / 2.54
 
-    # Calcola la dimensione in pixel per 16 cm
+    # Calculate the size in pixels for 16 cm
     size_in_pixel = int(pixels_per_cm * 16)
 
-    # Crea una nuova immagine con la dimensione calcolata
+    # Create a new image with the calculated size
     new_image = Image.new('RGB', (size_in_pixel, size_in_pixel), 'white')
 
-    # Ridimensiona l'immagine originale alla dimensione desiderata
+    # Resize the original image to the desired size
     resized_image = original_image.resize((size_in_pixel, size_in_pixel))
 
-    # Copia l'immagine ridimensionata nella nuova immagine
+    # Copy the resized image into the new image
     new_image.paste(resized_image, (0, 0))
 
-    # Crea un oggetto ImageDraw per disegnare sulla nuova immagine
+    # Create an ImageDraw object to draw on the new image
     draw = ImageDraw.Draw(new_image)
 
-    # Calcola la dimensione di ciascun quadratino
+    # Calculate the size of each square
     square_size = size_in_pixel // 16
 
-    # Imposta lo spessore e il colore dei tratti e dei numeri
+    # Set the thickness and color of the lines and numbers
     line_width = 5
     line_color = 'red'
     font_size = font_size = int(pixels_per_cm * 0.4)
 
-    # Carica un font con la dimensione desiderata
+    # Load a font with the desired size
     font = ImageFont.truetype("arial.ttf", font_size)
 
-    # Disegna la griglia con numerazione
+    # Draw the grid with numbering
     for i in range(16):
         for j in range(16):
             x = j * square_size
             y = (15 - i) * square_size
             draw.line([(x, 0), (x, size_in_pixel)], fill=line_color, width=line_width)
             draw.line([(0, y), (size_in_pixel, y)], fill=line_color, width=line_width)
-            draw.text((x + 2, y + 2), f'{j},{i}', fill=line_color, font=font)  # Scambia j con i
+            draw.text((x + 2, y + 2), f'{j},{i}', fill=line_color, font=font)  # Swap j with i
 
-    # Ottieni il percorso dell'immagine di origine
+    # Get the path of the original image
     image_dir = os.path.dirname(image_path)
 
-    # Salva l'immagine risultante nella stessa cartella dell'immagine di origine
-    new_image_path = os.path.join(image_dir, 'immagine_con_griglia.png')
+    # Save the resulting image in the same folder as the original image
+    new_image_path = os.path.join(image_dir, 'gridded_image.png')
     new_image.save(new_image_path)
-    print(f"L'immagine con la griglia è stata salvata come: {new_image_path}")
+    print(f"The image was saved as: {new_image_path}")
 
-# Richiedi il percorso dell'immagine da tastiera
-input_image_path = input("Inserisci il percorso dell'immagine: ")
+# Ask for the image path from the user
+input_image_path = input("Insert image path: ")
 draw_grid(input_image_path)
---
+++ readme.md
@@ -0,0 +1,20 @@
+Image Gridder
+Image Gridder is a Python program that takes an image file as input and adds a grid with numbering to it. The resulting image is saved in the same folder as the original image.
+
+Installation
+To use Image Gridder, you need to have Python 3 and the Pillow library installed. You can install Pillow using pip:
+``` pip install Pillow ```
+
+Usage
+To use Image Gridder, run the image-gridder.py file in a Python environment. The program will ask you to input the path of the image file you want to grid. Once you provide the path, the program will create a new image file with the grid and numbering added.
+
+Example
+Here's an example of how to use Image Gridder:
+```
+$ python image-gridder.py
+Insert image path: /path/to/image.jpg
+The image was saved as: /path/to/gridded_image.png
+```
+
+License
+Image Gridder is licensed under the MIT License. See the LICENSE file for more information.
